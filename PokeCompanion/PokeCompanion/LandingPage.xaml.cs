b3d0f435fd8d79cfc6b49d7ccfb9f2304b2e8e39
KuehneKevin <kuehne.imp@gmail.com>
2024-06-27 22:01:42 +0200

    Add files via upload
    Adding Visual Studio Project to Repo

---
+++ PokeCompanion/PokeCompanion/LandingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PokeCompanion
+{
+    /// <summary>
+    /// Interaction logic for LandingPage.xaml
+    /// </summary>
+    public partial class LandingPage : Window
+    {
+        public LandingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
